db8b9d9dc594018c3e752d3c74d7e5f580c6ab2d
praneetha16 <72999291+praneetha16@users.noreply.github.com>
2024-05-03 22:52:45 +0530

    Delete delete.py

--- delete.py
+++
@@ -1,9 +0,0 @@
-import requests
-def delete_todo():
-    url = "https://jsonplaceholder.typicode.com/todos/1"
-    response = requests.delete(url)
-    print(response.json())
-    print(response.status_code)
-
-if __name__ == "__main__":
-    delete_todo()
